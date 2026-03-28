c0a8f071e614a1669b3467c08a763b486e6d3c98
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-28 22:53:41 +0530

    GoCardlessClient.Resources.BankAuthorisations

---
+++ lib/gocardless_client/resources/bank_authorisations.ex
@@ -0,0 +1,54 @@
+defmodule GoCardlessClient.Resources.BankAuthorisations do
+  @moduledoc """
+  GoCardlessClient Bank Authorisations API.
+
+  See https://developer.gocardless.com/api-reference/#bank-authorisations for full documentation.
+  """
+
+  alias GoCardlessClient.{Client, Paginator, Resource}
+
+  @resource_key "bank_authorisations"
+  @base_path "/bank_authorisations"
+
+  @doc "Creates a new bank authorisations resource."
+  @spec create(Client.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def create(%Client{} = client, params, opts \\ []) do
+    Resource.post(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Retrieves a single bank authorisations by ID."
+  @spec get(Client.t(), String.t(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def get(%Client{} = client, id, opts \\ []) do
+    Resource.get(client, "#{@base_path}/#{id}", @resource_key, opts)
+  end
+
+  @doc "Updates a bank authorisations."
+  @spec update(Client.t(), String.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def update(%Client{} = client, id, params, opts \\ []) do
+    Resource.put(client, "#{@base_path}/#{id}", @resource_key, params, opts)
+  end
+
+  @doc "Lists bank_authorisations with optional filter params."
+  @spec list(Client.t(), map(), keyword()) ::
+          {:ok, %{items: [map()], meta: map()}}
+          | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def list(%Client{} = client, params \\ %{}, opts \\ []) do
+    Resource.list(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Returns a lazy `Stream` over all pages of bank_authorisations."
+  @spec stream(Client.t(), map(), keyword()) :: Enumerable.t()
+  def stream(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.stream(client, @base_path, params, @resource_key, opts)
+  end
+
+  @doc "Eagerly collects all bank_authorisations into a list across all pages."
+  @spec collect_all(Client.t(), map(), keyword()) ::
+          {:ok, [map()]} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def collect_all(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.collect(client, @base_path, params, @resource_key, opts)
+  end
+end
