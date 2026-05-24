a813542e452ad8430a0862338e76fe02e9241036
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-05-24 23:10:51 +0530

    updated

--- lib/gocardless_client/resources/subscriptions.ex
+++ lib/gocardless_client/resources/subscriptions.ex
@@ -40,7 +40,7 @@ defmodule GoCardlessClient.Resources.Subscriptions do
   @spec update(Client.t(), String.t(), map(), keyword()) ::
           {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
   def update(%Client{} = client, id, params, opts \\ []) do
-    Resource.put(client, "#{@base_path}/#{id}", @resource_key, params, opts)
+    Resource.update(client, "#{@base_path}/#{id}", @resource_key, params, opts)
   end
 
   @doc "Returns a page of subscriptions with optional filters."
