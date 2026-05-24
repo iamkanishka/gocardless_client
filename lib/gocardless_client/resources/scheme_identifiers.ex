b24d5d960e6cd72636d757257c2259517af76933
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-05-24 23:10:46 +0530

    updated

--- lib/gocardless_client/resources/scheme_identifiers.ex
+++ lib/gocardless_client/resources/scheme_identifiers.ex
@@ -28,7 +28,7 @@ defmodule GoCardlessClient.Resources.SchemeIdentifiers do
   @spec update(Client.t(), String.t(), map(), keyword()) ::
           {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
   def update(%Client{} = client, id, params, opts \\ []) do
-    Resource.put(client, "#{@base_path}/#{id}", @resource_key, params, opts)
+    Resource.update(client, "#{@base_path}/#{id}", @resource_key, params, opts)
   end
 
   @doc "Lists scheme_identifiers with optional filter params."
